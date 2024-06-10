def201489acf3374ea5b18abaeceac83b44b6d5d
Marwan Yassin <marwan.yassin@gmail.com>
2024-06-11 02:52:35 +0300

    umami

--- aitserve.py
+++ aitserve.py
@@ -242,8 +242,8 @@ async def head_id_handler(headers: dict, channel_id: str = None) -> None:
     print("HEAD", datetime.utcnow(), channel_id, headers)
 
     #resp = await get_by_channel_id(headers, channel_id, "HEAD")
-    r = requests.get(url=f"https://tait.wns.watch/app/{channel_id}", headers=headers)
-    print(r.ok, r.text, r.status_code)
+    #r = requests.get(url=f"https://tait.wns.watch/app/{channel_id}", headers=headers)
+    #print(r.ok, r.text, r.status_code)
     return Redirect(path="/sstracker")
 
 @get("/app/viewership/{channel_id:str}",  media_type=MediaType.HTML)
