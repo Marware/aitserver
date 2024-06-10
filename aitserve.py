1958c736d3231edfbf565068598155ec574618a4
Marwan Yassin <marwan.yassin@gmail.com>
2024-06-11 02:48:10 +0300

    umami

--- aitserve.py
+++ aitserve.py
@@ -237,7 +237,7 @@ async def get_id_handler(headers: dict, channel_id: str = None) -> dict[str, str
     return resp
 
 @head("/app/{channel_id:str}",  media_type=MediaType.HTML)
-async def head_id_handler(headers: dict, channel_id: str = None) -> dict[str, str]:
+async def head_id_handler(headers: dict, channel_id: str = None) -> None:
     print("HEAD", datetime.utcnow(), channel_id, headers)
 
     #resp = await get_by_channel_id(headers, channel_id, "HEAD")
